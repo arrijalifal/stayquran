f1c07ca6b0652e5540c106448a48a8385f3dbd31
arrijalifal <ifalgantengifal@gmail.com>
2023-07-21 14:42:19 +0700

    react fragment

--- pages/index.jsx
+++ pages/index.jsx
@@ -46,15 +46,17 @@ export default function Home() {
         <h1 className={`heading1 text-center`}>Keunggulan</h1>
         <div className='grid gap-5 grid-cols-2 text-center px-[4.5rem] pt-16'>
           {
-            Array.from({ length: 6 }).map(_ => {
+            Array.from({ length: 6 }).map((_, idx) => {
               return (
-                <div className='flex px-8 py-6 shadow-lg shadow-grey-400 rounded-xl'>
-                  <div className='w-20 h-20 rounded-full  border-2'></div>
-                  <div className='flex-1 ml-6 text-left'>
-                    <h1 className='heading2'>Lorem Ipsum</h1>
-                    <p className='mt-4 regulartext'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                <React.Fragment key={idx}>
+                  <div className='flex px-8 py-6 shadow-lg shadow-grey-400 rounded-xl'>
+                    <div className='w-20 h-20 rounded-full  border-2'></div>
+                    <div className='flex-1 ml-6 text-left'>
+                      <h1 className='heading2'>Lorem Ipsum</h1>
+                      <p className='mt-4 regulartext'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
                   </div>
-                </div>
+                </React.Fragment>
               )
             })
           }
@@ -64,15 +66,17 @@ export default function Home() {
         <h1 className='heading1 text-center pb-8'>Testimoni</h1>
         <div className='grid grid-cols-3 gap-5 px-36'>
           {
-            Array.from({ length: 3 }).map(_ => {
+            Array.from({ length: 3 }).map((_, idx) => {
               return (
-                <div className='shadow-xl shadow-grey-400 py-8'>
-                  <div className='w-40 h-40 rounded-full border mx-auto'></div>
-                  <h2 className='heading2 text-center my-4'>Lorem Ipsum</h2>
-                  <p className='px-8 text-justify'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam similique amet aut velit ullam, libero, animi sit cum temporibus dolores sed tenetur aliquid saepe blanditiis sequi minus quidem maxime ducimus!
-                  </p>
-                </div>
+                <React.Fragment key={idx}>
+                  <div className='shadow-xl shadow-grey-400 py-8'>
+                    <div className='w-40 h-40 rounded-full border mx-auto'></div>
+                    <h2 className='heading2 text-center my-4'>Lorem Ipsum</h2>
+                    <p className='px-8 text-justify'>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam similique amet aut velit ullam, libero, animi sit cum temporibus dolores sed tenetur aliquid saepe blanditiis sequi minus quidem maxime ducimus!
+                    </p>
+                  </div>
+                </React.Fragment>
               )
             })
           }
