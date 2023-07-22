f8105a3d996a2d883d777495b93f703a40f49b67
arrijalifal <ifalgantengifal@gmail.com>
2023-07-22 07:26:40 +0700

    changed height on some section

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,11 @@
 import Image from 'next/image';
 import style from '@/styles/Home.module.css';
 import DefaultNavBar from '@/components/DefaultNavBar';
-import icons from '@/utils/iconskeunggulan';
 import React from 'react';
 import Link from 'next/link';
+import keunggulan from '@/utils/benefits';
 
 export default function Home() {
-  const content = [
-    {
-      icon: icons.flexible,
-      title: 'Fleksibel',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    },
-    {
-      icon: icons.privacy,
-      title: 'Fleksibel',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    },
-    {
-      icon: icons.certificate,
-      title: 'Fleksibel',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    }
-  ]
   return (
     <div>
       <section className={`h-screen w-full flex flex-col`}>
@@ -42,18 +25,18 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section id='keunggulan' className='h-screen pt-3'>
+      <section id='keunggulan' className=' pt-3'>
         <h1 className={`heading1 text-center`}>Keunggulan</h1>
         <div className='grid gap-5 grid-cols-2 text-center px-[4.5rem] pt-16'>
           {
-            Array.from({ length: 6 }).map((_, idx) => {
+            keunggulan.map((k, idx) => {
               return (
                 <React.Fragment key={idx}>
-                  <div className='flex px-8 py-6 shadow-lg shadow-grey-400 rounded-xl'>
+                  <div className='flex px-8 py-6 shadow-lg shadow-grey-400 rounded-xl bg-[#F5F5F5]'>
                     <div className='w-20 h-20 rounded-full  border-2'></div>
                     <div className='flex-1 ml-6 text-left'>
-                      <h1 className='heading2'>Lorem Ipsum</h1>
-                      <p className='mt-4 regulartext'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                      <h1 className='heading2'>{k.title}</h1>
+                      <p className='mt-4 regulartext'>{k.description}</p>
                     </div>
                   </div>
                 </React.Fragment>
@@ -62,16 +45,21 @@ export default function Home() {
           }
         </div>
       </section>
-      <section id="testimoni" className='h-screen pt-8'>
+      <section id="testimoni" className=' pt-8'>
         <h1 className='heading1 text-center pb-8'>Testimoni</h1>
         <div className='grid grid-cols-3 gap-5 px-36'>
           {
             Array.from({ length: 3 }).map((_, idx) => {
               return (
                 <React.Fragment key={idx}>
-                  <div className='shadow-xl shadow-grey-400 py-8'>
-                    <div className='w-40 h-40 rounded-full border mx-auto'></div>
-                    <h2 className='heading2 text-center my-4'>Lorem Ipsum</h2>
+                  <div className='shadow-xl shadow-grey-400 py-8 rounded-xl bg-[#F5F5F5]'>
+                    <div className='w-40 h-40 mx-auto relative'>
+                      <Image src={'/images/download.jpeg'} fill='responsive' className='rounded-full'></Image>
+                    </div>
+                    <div className='my-4 text-center'>
+                      <h1 className='heading2'>Penguin Master</h1>
+                      <h2 className='italic mt-0.5'>Hewan, 2023</h2>
+                    </div>
                     <p className='px-8 text-justify'>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam similique amet aut velit ullam, libero, animi sit cum temporibus dolores sed tenetur aliquid saepe blanditiis sequi minus quidem maxime ducimus!
                     </p>
